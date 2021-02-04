56292c065534e7795dbcb68d4fa5f003ece57ac5
ruchika110 <ruchika149@gmail.com>
2021-02-03 17:42:46 -0800

    web file

---
+++ get.js
@@ -0,0 +1,12 @@
+// var element; 
+// element =document.body; 
+// element=document.getElementById("ruchika").innerText; 
+// element=document.getElementById("ruchika").innerHTML;
+ // element=document.getElementById("ruchika").getAttribute("style"); 
+ // element=document.getElementById("ruchika").getAttributeNode("style");
+  // element=document.getElementById("ruchika").attributes; 
+  // element=document.getElementById("ruchika").attributes[0]; 
+  // console.log(element);
+
+  var element;
+console.log(element);
---
+++ set.js
@@ -0,0 +1,9 @@
+// document.getElementById("ruchika").innerHTML="<center><h1>ITM COLLEGE</h1></center>"
+
+// document.getElementById("ruchika").setAttribute("style","border:10px dotted yellow");
+
+// document.getElementById("ruchika").removeAttribute("style");
+
+// document.querySelector("#ruchika").innerHTML ="<center><h1>WOW</h1></center>";
+
+element=document.querySelector("#ruchika")
